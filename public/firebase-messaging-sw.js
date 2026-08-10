f89b3736f9f052516e3790e7b9c2ce37b704bd78
yawadomako123 <adomakoy42@gmail.com>
2026-08-10 12:36:45 +0000

    firebase: point web push to konura-4450d

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -4,12 +4,12 @@ importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js'
 importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-messaging-compat.js');
 
 firebase.initializeApp({
-  apiKey: "AIzaSyB8TmubkjGN0DMdtzFx7qumGFZhMt8Uh88",
-  authDomain: "niena-f0339.firebaseapp.com",
-  projectId: "niena-f0339",
-  storageBucket: "niena-f0339.firebasestorage.app",
-  messagingSenderId: "137439028204",
-  appId: "1:137439028204:web:c2c0bae77e6433900116c9",
+  apiKey: "AIzaSyB9qIwhzOlXvHrJJpxI9fzKhvGjtRbN7ws",
+  authDomain: "konura-4450d.firebaseapp.com",
+  projectId: "konura-4450d",
+  storageBucket: "konura-4450d.firebasestorage.app",
+  messagingSenderId: "333244137876",
+  appId: "1:333244137876:web:cbe408322a062e399d0f81",
 });
 
 const messaging = firebase.messaging();
